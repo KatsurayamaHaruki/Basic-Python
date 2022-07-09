d580d61d7f8dd080560743cf225c3981ce9575dc
Katsurayama Haruki <h.katsurayama@4nonome.com>
2022-07-09 13:06:38 +0900

    change AOJ/02_C

--- AOJ/02/02_C.py
+++ AOJ/02/02_C.py
@@ -1,13 +1,3 @@
-a, b, c = map(int, input().split())
-
-m1 = min(a, b, c)
-m3 = max(a, b, c)
-
-if (m1 == a or m3 == a) and (m1 == b or m3 == b):
-    m2 = c
-elif(m1 == a or m3 == a) and (m1 == c or m3 == c):
-    m2 = b
-else:
-    m2 = a
-
-print(m1, m2, m3)
+s = list(map(int, input().split()))
+s.sort()
+print(s[0], s[1], s[2])
